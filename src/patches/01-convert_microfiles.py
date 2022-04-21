02476ecd5f54a376caef901b5b18f61d1f3ffa44
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-21 10:50:42 +0200

    fix loading script

--- src/patches/01-convert_microfiles.py
+++ src/patches/01-convert_microfiles.py
@@ -11,38 +11,40 @@ data = defaultdict(lambda: {"cls": [], "mean": [], "haddamard": [], "sum": []})
 MODEL = "BERT"
 
 for layer in tqdm(range(12)):
-    for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/CLS/{layer}/*.npy"):
-        fname = f.split("/")[-1].split(".npy")[0]
-        with open(f, "rb") as f:
-            v = np.load(f)
-            data[fname]["cls"].append(v)
-
-
-    for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/Token_Aggregation/Haddamard/{layer}/*.npy"):
-        fname = f.split("/")[-1].split(".npy")[0]
-        with open(f, "rb") as f:
-            v = np.load(f)
-            data[fname]["haddamard"].append(v)
-
-    for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/Token_Aggregation/Mean/{layer}/*.npy"):
-        fname = f.split("/")[-1].split(".npy")[0]
-        with open(f, "rb") as f:
-            v = np.load(f)
-            data[fname]["mean"].append(v)
-
-    for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/Token_Aggregation/Sum/{layer}/*.npy"):
-        fname = f.split("/")[-1].split(".npy")[0]
-        with open(f, "rb") as f:
-            v = np.load(f)
-            data[fname]["sum"].append(v)
-
-    # a hack to be included in tqdm
-    if layer == 0:
-        for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/pooler/*.npy"):
+    for amb_mode in ["ambiguous", "unambiguous"]:
+        for f in glob(f"data/Ambiguity/{MODEL}/{amb_mode}_representations/CLS/{layer}/*.npy"):
             fname = f.split("/")[-1].split(".npy")[0]
             with open(f, "rb") as f:
                 v = np.load(f)
-                data[fname]["pooler"] = v
+                data[fname]["cls"].append(v)
+                data[fname]["amb"] = amb_mode == "ambiguous"
+
+
+        for f in glob(f"data/Ambiguity/{MODEL}/{amb_mode}_representations/Token_Aggregation/Haddamard/{layer}/*.npy"):
+            fname = f.split("/")[-1].split(".npy")[0]
+            with open(f, "rb") as f:
+                v = np.load(f)
+                data[fname]["haddamard"].append(v)
+
+        for f in glob(f"data/Ambiguity/{MODEL}/{amb_mode}_representations/Token_Aggregation/Mean/{layer}/*.npy"):
+            fname = f.split("/")[-1].split(".npy")[0]
+            with open(f, "rb") as f:
+                v = np.load(f)
+                data[fname]["mean"].append(v)
+
+        for f in glob(f"data/Ambiguity/{MODEL}/{amb_mode}_representations/Token_Aggregation/Sum/{layer}/*.npy"):
+            fname = f.split("/")[-1].split(".npy")[0]
+            with open(f, "rb") as f:
+                v = np.load(f)
+                data[fname]["sum"].append(v)
+
+        # a hack to be included in tqdm
+        if layer == 0:
+            for f in glob(f"data/Ambiguity/{MODEL}/ambiguous_representations/pooler/*.npy"):
+                fname = f.split("/")[-1].split(".npy")[0]
+                with open(f, "rb") as f:
+                    v = np.load(f)
+                    data[fname]["pooler"] = v
 
 data = [
     {"name": fname, **vals}
@@ -52,5 +54,5 @@ data = [
 print("Loaded", len(data), "data")
 print("Every entry has the following keys:", data[0].keys())
 
-with open("data/ambiguity.pkl", "wb") as f:
+with open(f"data/ambiguity_{MODEL}.pkl", "wb") as f:
     pickle.dump(data, f)
