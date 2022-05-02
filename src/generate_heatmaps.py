5dbab5e507a2da61b5515dd3904920efc90756c8
kijaaniki <sundroid.b@gmail.com>
2022-05-02 14:16:23 +0200

    adding COCO/EMMT/CoLA heatmap

--- src/generate_heatmaps.py
+++ src/generate_heatmaps.py
@@ -19,6 +19,7 @@ def extract_summary(rep_loc):
             test_scores = [x[1] for x in target]
             if max(test_scores)>max_score_global:
                 max_score_global=max(test_scores)
+            
         else:
             for layer in range(13):
                 target = data[rep_type][str(layer)]
@@ -26,70 +27,81 @@ def extract_summary(rep_loc):
                 max_score = max(test_scores)
                 if max_score>max_score_global:
                     max_score_global = max_score
+                
     return max_score_global
 
 def summary_stats(data_loc):
     dataset_name = data_loc.split("/")[-1]
     
-    scores = []
-    
+    max_scores = []    
 
     tf_idf = float(read_tfidf(dataset_name)) 
-    scores.append(tf_idf)
+    max_scores.append(tf_idf)
 
     BERT = os.path.join(data_loc,"mlp_BERT.json")
     max_score = extract_summary(BERT)
-    scores.append(max_score)
-
+    max_scores.append(max_score)
+    
     GPT2 = os.path.join(data_loc,"mlp_GPT2.json")
     max_score = extract_summary(GPT2)
-    scores.append(max_score)
-
+    max_scores.append(max_score)
+   
     SBERT = os.path.join(data_loc,"mlp_SBERT.json")
     max_score = extract_summary(SBERT)
-    scores.append(max_score)
-
-    return scores
+    max_scores.append(max_score)
+    
+    return max_scores
 
 
 
 def ambiguous():
-    case_scores = []
+    case_scores_max = []
+    
     ambiguous_location = os.path.join(os.getcwd(),os.path.join("computed","Ambiguity"))
     
     COCO = os.path.join(ambiguous_location,"COCO")
-    scores = summary_stats(COCO)
-    case_scores.append(scores)
+    max_scores  = summary_stats(COCO)
+    case_scores_max.append(max_scores)
+    
     
     EMMT = os.path.join(ambiguous_location,"EMMT")
-    scores = summary_stats(EMMT)
-    case_scores.append(scores)
-    
-    return case_scores
+    max_scores = summary_stats(EMMT)
+    case_scores_max.append(max_scores)
+        
+    return case_scores_max
     
 def CoLA():
-    case_scores = []
-    ambiguous_location = os.path.join(os.getcwd(),os.path.join("computed","Grammaticality"))
+    case_scores_max = []
+
+    data_location = os.path.join(os.getcwd(),os.path.join("computed","Grammaticality"))
     
-    CoLA = os.path.join(ambiguous_location,"CoLA")
-    scores = summary_stats(CoLA)
-    case_scores.append(scores)
-    
-    
-    return case_scores
+    CoLA = os.path.join(data_location,"CoLA")
+    max_scores = summary_stats(CoLA)
+    case_scores_max.append(max_scores)
+
+    return case_scores_max
     
 
 Dataset_names = ["COCO","EMMT","CoLA"]
-All_data = []
-amb_data_scores = ambiguous() 
-All_data = All_data + amb_data_scores
-CoLA_data_scores = CoLA() 
-All_data = All_data + CoLA_data_scores
-Dataset = np.array(All_data)
-print(Dataset.shape)
-text = np.repeat(np.array(["T","B","G","S"]),len(Dataset_names))
-print(text.shape)
-# fig, ax = plt.subplots()
-#   # drawing heatmap on current axes
-# ax = sns.heatmap(Dataset, annot=text, fmt="")
-# plt.savefig("Heatmap")
+All_data_max = []
+
+amb_data_scores_max = ambiguous() 
+All_data_max = All_data_max + amb_data_scores_max
+
+CoLA_data_scores_max = CoLA() 
+All_data_max = All_data_max + CoLA_data_scores_max
+
+
+Dataset_max = np.array(All_data_max)
+x_axis_labels = ["TF-IDF","BERT","GPT2","SBERT"] # labels for x-axis
+y_axis_labels = Dataset_names # labels for y-axis
+
+fig, ax = plt.subplots()
+  # drawing heatmap on current axes
+ax = sns.heatmap(Dataset_max, xticklabels=x_axis_labels, yticklabels=y_axis_labels, fmt="")
+plt.title("Heatmap: Max")
+plt.savefig("computed/Heatmap_max.pdf")
+
+plt.clf()
+plt.close()
+
